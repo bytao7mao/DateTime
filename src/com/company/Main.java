6c5c70175db8ce1dab9b86e9560f87694892bed2
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2017-12-25 14:05:40 +0200

    ff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,31 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String country = sc.nextLine();
+        DateFormat df2;
         DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
         Date dt = new Date();
         System.out.println("Date in MM/dd/yyyy format: " + df.format(dt));
-        DateFormat df2 = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
-        System.out.println("France time: " + df2.format(dt));
+
+        if(country.equalsIgnoreCase("france")){
+            df2 = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
+        }
+        else if(country.equalsIgnoreCase("england")){
+            df2 = DateFormat.getDateInstance(DateFormat.LONG, Locale.UK);
+        }
+        else if(country.equalsIgnoreCase("china")){
+            df2 = DateFormat.getDateInstance(DateFormat.LONG, Locale.CHINA);
+        }
+        else {
+            df2 = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
+        }
+        System.out.println("Your country " + country + " time is : " + df2.format(dt));
     }
 }
